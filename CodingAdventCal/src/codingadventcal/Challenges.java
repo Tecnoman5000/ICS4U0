04dd91bc89aaa3ec462d51cb75a58e0dcc263cff
tecnoman5000 <theyoungermorey@gmail.com>
2015-12-04 13:23:02 -0500

    Day 4 Complete

--- CodingAdventCal/src/codingadventcal/Challenges.java
+++ CodingAdventCal/src/codingadventcal/Challenges.java
@@ -7,15 +7,17 @@ package codingadventcal;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.security.MessageDigest;
 
 /**
  *
  * @author tecnologic
  */
-public class Day1_1 {
+public class Challenges {
             
     /**
      * @param args the command line arguments
@@ -33,7 +35,63 @@ public class Day1_1 {
         System.out.println("Total Ribbon Needed: " + d2Results.get(1));
         
         System.out.println("----------- Day Three ------------");
+        // Challenge 1 complete, 2 incomplete (broken)
         ArrayList<Integer> d3Results = dayThree("d3.txt");
+        
+        System.out.println("----------- Day Four ------------");
+        ArrayList<Integer> d4Results = dayFour("ckczppom");
+        System.out.println("Answer 1: " + d4Results.get(0));
+        System.out.println("Answer 2: " + d4Results.get(1));
+    }
+    
+    public static ArrayList<Integer> dayFour(String input){
+        ArrayList<Integer> results = new ArrayList<>();
+        String stringMD5 = "";
+        
+        boolean ansFnd1 = false;
+        boolean ansFnd2 = false;
+        int index = 1;
+        while(!ansFnd1 && !ansFnd2){
+            stringMD5 = getMD5(input + index);
+            if(stringMD5.substring(0, 5).equals("00000")){
+                results.add(index);
+                System.out.println(index);
+                ansFnd1 = true;
+            }
+            if(stringMD5.substring(0, 6).equals("000000")){
+                results.add(index);
+                System.out.println(index);
+                ansFnd2 = true;
+            }
+            if(index%1000 == 0){
+                //System.out.println("Tried Between 0 - " + index);
+            }
+            index++;
+        }
+        
+        //System.out.println("MD5 hash: " + stringMD5 + " -- Answer: " + answer);
+        
+        return results;
+    }
+    
+    public static String getMD5(String input){
+        String hashtext = "";
+        try{
+            MessageDigest m = MessageDigest.getInstance("MD5");
+            m.reset();
+            m.update(input.getBytes());
+            byte[] digest = m.digest();
+            BigInteger bigInt = new BigInteger(1,digest);
+            hashtext = bigInt.toString(16);
+            // Now we need to zero pad it if you actually want the full 32 chars.
+            while(hashtext.length() < 32 ){
+              hashtext = "0"+hashtext;
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+        return hashtext;
     }
     
     public static ArrayList<Integer> dayThree(String fileName){
@@ -53,35 +111,57 @@ public class Day1_1 {
         }
         System.out.println("Number of Directions: " + directions.size());
         
+        int houses = getHouses(directions);
+        System.out.println("Houses w/ > 0 presents: " + houses);
+        
+        ArrayList<Character> dirSanta = new ArrayList<>();
+        ArrayList<Character> dirRobo = new ArrayList<>();
+
+        boolean robo = false;
+        for(char move : directions){
+            if(!robo){
+                dirSanta.add(move);
+                robo = true;
+            }else{
+                dirRobo.add(move);
+                robo = false;
+            }
+        }
+        
+        int housesSanta = getHouses(dirSanta);
+        int housesRobo = getHouses(dirRobo);
+        int housesTotal = housesSanta + housesRobo + 1;
+        System.out.println("Total Houses: " + housesTotal);
+        
+        return results;
+    }
+    public static int getHouses(ArrayList<Character> dir){
         ArrayList<int[]> locs = new ArrayList<>();
         int houses = 0;
-        for(char move : directions){
-            if(locs.isEmpty()){
-                System.out.println("Locs is Empty");
-                int[] initLoc = {0,0};
-                locs.add(calcMove(initLoc, move));
+        int[] previous = {0, 0};
+        for(char move : dir){
+            int[] current = calcMove(previous, move);
+            //System.out.println("Locs Size: " + locs.size() + " : " + Arrays.toString(previous));
+            //System.out.println(locs.contains(current) + " vs " + containsLoc(current, locs));
+            if(!containsLoc(current, locs)){
+                //System.out.println("Loc added");
+                locs.add(current);
                 houses++;
             }else{
-                int[] current = calcMove(locs.get(locs.size()-1), move);
-                System.out.println(locs.contains(current) + " vs " + containsLoc(current, locs));
-                if(!locs.contains(current)){
-                    locs.add(current);
-                    houses++;
-                }
+                //System.out.println("Same, Loc not added");
             }
+            previous = current;
             //System.out.println("Num House: " + houses);
             
         }
-        System.out.println("Houses w/ > 0 presents: " + houses);
-        
-        return results;
+        return houses;
     }
     
     public static boolean containsLoc(int[] current, ArrayList locs){
         boolean contains = false;
         ArrayList<int []> givenLocs = locs;
         for(int[] loc : givenLocs){
-            System.out.println(loc[0] + "," + loc[1] +" vs " + current[0] + "," + current[1]);
+            //System.out.println(loc[0] + "," + loc[1] +" vs " + current[0] + "," + current[1]);
             if(loc[0] == current[0] && loc[1] == current[1]){
                 contains = true;
             }
@@ -90,8 +170,10 @@ public class Day1_1 {
     }
     
     public static int[] calcMove(int[] loc, char move){
-        int[] previous = loc;
-        System.out.print("Previous: " + Arrays.toString(previous));
+        int[] previous = new int[2];
+        previous[0] = loc[0];
+        previous[1] = loc[1];
+        //System.out.print("Previous: " + Arrays.toString(previous));
         if(move == '<'){
             previous[0]--;
         }
@@ -104,8 +186,8 @@ public class Day1_1 {
         if(move == 'v'){
             previous[1]--;
         }
-        System.out.println(" Move: " + move + " Current: " 
-            + Arrays.toString(previous));
+        //System.out.println(" Move: " + move + " Current: " 
+        //    + Arrays.toString(previous));
         return previous;
     }
     
